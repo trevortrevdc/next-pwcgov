6d6c57fd0dd2357a9b34f4bcc77ab4333ef03ff2
Trevor Johnson <tajohnson@pwcgov.org>
2018-09-06 16:52:01 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Next - Prince William County',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#005eb8',
+        theme_color: '#005eb8',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from "../images/logo.png"
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      background: '#005eb8',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.0875rem 0 0 0',
+      }}
+    >
+      <img src={logo} alt="Logo" height="50" />
+    </div>
+  </div>
+)
+
+export default Header
